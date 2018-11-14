14d18ca9d5a4fdb810f750265407acce4d9c3497
wangding <wangding#123>
2018-11-14 23:00:11 +0800

    初次导入

---
+++ e3-manager/e3-manager-dao/src/main/java/org/e3/manager/dao/App.java
@@ -0,0 +1,13 @@
+package org.e3.manager.dao;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
